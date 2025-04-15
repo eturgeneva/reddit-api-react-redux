83e6a0da0ef09338084d9bb4804a549daf55202e
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-15 09:51:03 +0200

    add ArticlePreviews feature and import it into Timeline component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ArticlePreviews from '../features/ArticlePreviews.js';
 
 export default function Timeline() {
     return (
         <div className="timeline">
             <h1>Timeline</h1>
+            <ArticlePreviews />
         </div>
     )
 }
