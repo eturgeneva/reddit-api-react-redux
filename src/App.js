659fc037e9049719792ad5a03e325a33557c3654
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-15 09:38:09 +0200

    add basic app structure including header, search, timeline and subreddits components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header.js';
+import Timeline from './components/Timeline.js';
+import Subreddits from './components/Subreddits.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Timeline />
+        <Subreddits />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Search from '../features/Search.js';
+
+export default function Header() {
+    return (
+        <header className="App-header">
+            <div className="App-logo">Logo</div>
+            <Search />
+        </header>
+    )
+}
