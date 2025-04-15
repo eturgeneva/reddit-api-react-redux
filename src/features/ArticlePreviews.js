bfdbe0e00ebe0fc2f2ade91e7444dc783d300cdd
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-15 13:27:17 +0200

    add test mock for article preview at index 3 of the articles response array and rendered its preview inside ArticlePreviews.js

--- src/features/ArticlePreviews.js
+++ src/features/ArticlePreviews.js
@@ -19,7 +19,7 @@ export default function ArticlePreviews() {
         })
     }, []);
 
-    console.log('turtleFacts article preview', articlePreview);
+    console.log('turtleFacts article previews', articlePreview);
     if (!articlePreview) {
         return (
             <div>No data fetched</div>
@@ -41,6 +41,21 @@ export default function ArticlePreviews() {
                     <div>Upvotes: {articlePreview.data.children[0].data.ups}</div>
                     <div>Downvotes: {articlePreview.data.children[0].data.downs}</div>
                     <div>Comments: {articlePreview.data.children[0].data.num_comments}</div>
+                </div>
+                <div>
+                    <h2>Test response preview 03 index</h2>
+                    <h1>Post preview info</h1>
+                    <div>User: {articlePreview.data.children[3].data.author}</div>
+                    <div>Link to the post: {articlePreview.data.children[3].data.permalink}</div>
+                    <div>Subreddit: {articlePreview.data.children[3].data.subreddit}</div>
+                    <div>Thumbnail: {articlePreview.data.children[3].data.thumbnail}</div>
+                    <img className="postThumbnail" src={articlePreview.data.children[3].data.thumbnail}/>
+                    <div>Title: {articlePreview.data.children[3].data.title}</div>
+                    {/* <img className="postImage" src={articlePreview.data.children[3].data.url}/> */}
+                    {articlePreview.data.children[3].data.url}
+                    <div>Upvotes: {articlePreview.data.children[3].data.ups}</div>
+                    <div>Downvotes: {articlePreview.data.children[3].data.downs}</div>
+                    <div>Comments: {articlePreview.data.children[3].data.num_comments}</div>
                 </div>
             </div>
             <Article />
