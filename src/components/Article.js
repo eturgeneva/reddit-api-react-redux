8b0b443105ed7874252086265833942c03971498
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-15 13:04:21 +0200

    add rendering of article content inside Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,33 +2,45 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 // import turtleFacts from '../data/turtleFacts.json';
 
-export default function Article () {
-    const [response, setResponse] = useState(null);
+export default function Article() {
+    const [articleContent, setArticleContent] = useState(null);
 
     useEffect(() => {
         // Add best to sort by best
         // fetch('https://www.reddit.com/r/TurtleFacts/.json')
-        fetch('/mocks/TurtleFacts.json')
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(responseObj => {
-            setResponse(responseObj);
-        })
+        fetch('/mocks/TurtleFacts_best_01.json')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(responseObj => {
+                setArticleContent(responseObj);
+            })
     }, [])
 
-    console.log('turtleFacts json', response);
-    if (!response) {
+    console.log('turtleFacts article content', articleContent);
+    if (!articleContent) {
         return (
-            <div>No data fetched</div>
+            <div>No article content fetched</div>
         )
     }
 
     return (
         <>
-         <h2>Post expanded</h2>
+            <h2>Post expanded (stateArray[0].sameAsArticlePreview!)</h2>
+   
+            <div>User: {articleContent[0].data.children[0].data.author}</div>
+            <div>Link to the post: {articleContent[0].data.children[0].data.permalink}</div>
+            <div>Subreddit: {articleContent[0].data.children[0].data.subreddit}</div>
+            <div>Thumbnail: {articleContent[0].data.children[0].data.thumbnail}</div>
+            <img className="postThumbnail" src={articleContent[0].data.children[0].data.thumbnail} />
+            <div>Title: {articleContent[0].data.children[0].data.title}</div>
+            {/* <img className="postImage" src={articleContent[0].data.children[0].data.url}/> */}
+            <div>Upvotes: {articleContent[0].data.children[0].data.ups}</div>
+            <div>Downvotes: {articleContent[0].data.children[0].data.downs}</div>
+            <div>Comments: {articleContent[0].data.children[0].data.num_comments}</div>
+
         </>
     )
 }
