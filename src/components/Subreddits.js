b3dcbdb9f7430a5fe6d876943352b531d0195c68
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-16 15:51:22 +0200

    update handleClick function inside Subreddits component

--- src/components/Subreddits.js
+++ src/components/Subreddits.js
@@ -4,25 +4,16 @@ import { useState, useEffect } from 'react';
 export default function Subreddits(props) {
     const [subreddits, setSubreddits] = useState(['TurtleFacts']);
 
-    // function clickHandler(e) {
-    //     console.log(e.target.value);
-    //     props.redditClient.fetchArticlePreviews(e.target.value)
-    // }
+    function handleClick(e) {
+        props.redditClient.fetchArticlePreviews(e.target.innerText)
+    }
 
     return (
         <div className="subreddits">
             <h2>Subreddits</h2>
             {subreddits.length > 0 && subreddits.map((subreddit, index) => {
                 return (
-                    <div className="subreddit"
-                         value={subreddit}
-                         onClick={(e) => {
-                            console.log(e.target.innerText);
-                            console.log(e.target.value);
-                            console.log(e.target);
-                            console.log(e);
-                            props.redditClient.fetchArticlePreviews(e.target.innerText);
-                        }}>{subreddit}</div>
+                    <div className="subreddit" onClick={handleClick}>{subreddit}</div>
                 )
             })}
         </div>
