9489321036abc21b425db8538b4e7f4c3c600600
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-16 12:10:05 +0200

    create DataRender component and pass props to it from ArticlePreviews.js

---
+++ src/components/DataRender.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function ContentsRender(props) {
+    const dataToRender = props.dataToRender;
+    console.log('dataToRender', dataToRender);
+
+    return (
+        <>
+            {dataToRender && dataToRender.map((dataPiece, index) => {
+                return (
+                    <div className="article" key={index}>
+                        <div>{dataPiece.data.author}</div>
+                        <div>{dataPiece.data.permalink}</div>
+                        <div>{dataPiece.data.subreddit}</div>
+                        <div>Thumbnail: {dataPiece.data.thumbnail}</div>
+                        <img className="articleThumbnail" src={dataPiece.data.thumbnail}></img>
+                        <img className="articleImage" src={dataPiece.data.url}></img>
+                        <div>Upvotes: {dataPiece.data.ups}</div>
+                        <div>Downvotes: {dataPiece.data.downs}</div>
+                        <div>Commets: {dataPiece.data.num_comments}</div>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
--- src/features/ArticlePreviews.js
+++ src/features/ArticlePreviews.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Article from '../components/Article';
+import ContentsRender from '../components/DataRender';
 
 export default function ArticlePreviews(props) {
     const [articlePreviews, setArticlePreviews] = useState(null);
@@ -10,7 +11,8 @@ export default function ArticlePreviews(props) {
         props.redditClient.fetchPostsPreviews('TurtleFacts')
         .then(responseObj => {
             console.log('responseObj in ArticlePreviews', responseObj);
-            setArticlePreviews(responseObj);
+            // setArticlePreviews(responseObj);
+            setArticlePreviews(responseObj.data.children);
         })
     }, []);
 
@@ -24,53 +26,7 @@ export default function ArticlePreviews(props) {
 
     return (
         <div>
-            <div className="articlePreview">
-                <ul>
-                    {articlePreviews && articlePreviews.data.children.map((article, index) => {
-                        return (
-                            <div className="article" key={index}>
-                                <div>{article.data.author}</div>
-                                <div>{article.data.permalink}</div>
-                                <div>{article.data.subreddit}</div>
-                                <div>Thumbnail: {article.data.thumbnail}</div>
-                                <img className="articleThumbnail" src={article.data.thumbnail}></img>
-                                {/* <img className="articleImage" src={article.data.url}></img> */}
-                                <div>Upvotes: {article.data.ups}</div>
-                                <div>Downvotes: {article.data.downs}</div>
-                                <div>Commets: {article.data.num_comments}</div>
-                            </div>
-                        )
-                    })}
-                </ul>
-
-                <div className="article">
-                    <h1>Post preview info</h1>
-                    <div>User: {articlePreviews.data.children[0].data.author}</div>
-                    <div>Link to the post: {articlePreviews.data.children[0].data.permalink}</div>
-                    <div>Subreddit: {articlePreviews.data.children[0].data.subreddit}</div>
-                    <div>Thumbnail: {articlePreviews.data.children[0].data.thumbnail}</div>
-                    <img className="postThumbnail" src={articlePreviews.data.children[0].data.thumbnail}/>
-                    <div>Title: {articlePreviews.data.children[0].data.title}</div>
-                    <div>Upvotes: {articlePreviews.data.children[0].data.ups}</div>
-                    <div>Downvotes: {articlePreviews.data.children[0].data.downs}</div>
-                    <div>Comments: {articlePreviews.data.children[0].data.num_comments}</div>
-                </div>
-                <div>
-                    <h2>Test response preview 03 index</h2>
-                    <h1>Post preview info</h1>
-                    <div>User: {articlePreviews.data.children[3].data.author}</div>
-                    <div>Link to the post: {articlePreviews.data.children[3].data.permalink}</div>
-                    <div>Subreddit: {articlePreviews.data.children[3].data.subreddit}</div>
-                    <div>Thumbnail: {articlePreviews.data.children[3].data.thumbnail}</div>
-                    <img className="postThumbnail" src={articlePreviews.data.children[3].data.thumbnail}/>
-                    <div>Title: {articlePreviews.data.children[3].data.title}</div>
-                    {/* <img className="postImage" src={articlePreview.data.children[3].data.url}/> */}
-                    {articlePreviews.data.children[3].data.url}
-                    <div>Upvotes: {articlePreviews.data.children[3].data.ups}</div>
-                    <div>Downvotes: {articlePreviews.data.children[3].data.downs}</div>
-                    <div>Comments: {articlePreviews.data.children[3].data.num_comments}</div>
-                </div>
-            </div>
+            <ContentsRender dataToRender={articlePreviews}/>
             <Article />
         </div>
     )
