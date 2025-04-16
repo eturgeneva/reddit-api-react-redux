f9a0d5870267cce38687139ca2b0e974dbb83bf4
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-16 14:04:13 +0200

    wip CurrentArticle contents and comments rendering

--- src/components/CurrentArticle.js
+++ src/components/CurrentArticle.js
@@ -4,42 +4,53 @@ import DataRender from './DataRender';
 
 export default function CurrentArticle(props) {
     const [articleContent, setArticleContent] = useState(null);
+    const [articleComments, setArticleComments] = useState(null);
 
     useEffect(() => {
         props.redditClient.fetchSelectedArticle('TurtleFacts_best_01')
         .then(responseObj => {
             setArticleContent(responseObj[0].data.children);
+            setArticleComments(responseObj[1].data.children);
         })
     }, [])
     // Test index 03, remove later:
-    const [article03Content, setArticle03Content] = useState(null);
-    useEffect(() => {
-        fetch('/mocks/TurtleFacts_best_03.json')
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-            })
-            .then(responseObj => {
-                setArticle03Content(responseObj);
-            })
-    }, [])
+    // const [article03Content, setArticle03Content] = useState(null);
+    // useEffect(() => {
+    //     fetch('/mocks/TurtleFacts_best_03.json')
+    //         .then(response => {
+    //             if (response.ok) {
+    //                 return response.json();
+    //             }
+    //         })
+    //         .then(responseObj => {
+    //             setArticle03Content(responseObj);
+    //         })
+    // }, [])
 
     console.log('turtleFacts article content', articleContent);
-    console.log('turtleFacts article 03 content', article03Content);
+    console.log('current article comments', articleComments);
+    // console.log('turtleFacts article 03 content', article03Content);
 
-    if (!articleContent || !article03Content) {
+    if (!articleContent) {
         return (
             <div>No article content fetched</div>
+        )
+    }
+    if (!articleComments) {
+        return (
+            <div>This article doesn't have any comments yet'</div>
         )
     }
 
     return (
         <>
-        <h3>Rendering with DataRender component:</h3>
+        <h3>Current article rendering with DataRender component:</h3>
         <DataRender dataToRender={articleContent}/>
-        
-        <div>
+
+        <h2>Current article comments rendering:</h2>
+        <DataRender dataToRender={articleComments} />
+
+        {/* <div>
             <h2>Comments</h2>
             <div>Author: {article03Content[1].data.children[0].data.author}</div>
             <div>Comment: {article03Content[1].data.children[0].data.body}</div>
@@ -47,7 +58,7 @@ export default function CurrentArticle(props) {
 
             <div>Author: {article03Content[1].data.children[1].data.author}</div>
             <div>Comment: {article03Content[1].data.children[1].data.body}</div>
-        </div>
+        </div> */}
 
         </>
     )
