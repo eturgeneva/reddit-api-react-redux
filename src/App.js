a40b7596fbfe3707a3d15c581a3694b81c12cdb5
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-17 09:58:57 +0200

    wip routing

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const redditClient = new Reddit();
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Homepage redditClient={redditClient}/> }>
+    {/* <Route path="timeline" element ={<Timeline redditClient={redditClient}/>} /> */}
     <Route path="/TurtleFacts" element={ <ArticlePreviews redditClient={redditClient}/> }/>
   </Route>
 )
