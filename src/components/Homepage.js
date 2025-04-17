29848020fefcd14c8b08c7bfb8bf48f3eb11d986
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-17 12:47:58 +0200

    cleanup code in Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 import Header from "./Header";
-// import Timeline from "./Timeline";
-import ArticlePreviews from "../features/ArticlePreviews";
 import Subreddits from "./Subreddits";
 import { Outlet } from "react-router-dom";
 
@@ -11,8 +9,6 @@ export default function Homepage(props) {
         <>
             <Header />
             <main>
-                {/* <Timeline redditClient={props.redditClient}/> */}
-                {/* <ArticlePreviews redditClient={props.redditClient}/> */}
                 <Outlet />
                 <Subreddits redditClient={props.redditClient}/>
             </main>
