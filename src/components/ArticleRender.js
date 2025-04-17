16b70806d669e601a78d5ec38509f74727873bfe
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-17 17:35:54 +0200

    display images from gallery in posts

--- src/components/ArticleRender.js
+++ src/components/ArticleRender.js
@@ -20,6 +20,12 @@ export default function ArticleRender(props) {
                             <div>Upvotes: {dataPiece.data.ups}</div>
                             <div>Downvotes: {dataPiece.data.downs}</div>
                             <div>Comments: {dataPiece.data.num_comments}</div>
+                            {/* <img src="https://preview.redd.it/6sl38rdona471.jpg?width=640&crop=smart&auto=webp&s=159a5bb2b3b66b4561783c8e2455d5e66fce719b"/> */}
+                            {/* <img src="https://preview.redd.it/6sl38rdona471.jpg?width=320&amp;crop=smart&amp;auto=webp&amp;s=95a685cc6db50974ad95f0a571b20b58ecdc29f0"/> */}
+                            <img src={Object.values(dataPiece.data.media_metadata)[0].p[3].u.replaceAll('&amp;', '&')}/>
+                            <div>{Object.values(dataPiece.data.media_metadata)[0].p[3].u.replace('&amp;', '&')}</div>
+                            <img src="https://preview.redd.it/voj6lldona471.jpg?width=640&amp;crop=smart&amp;auto=webp&amp;s=d69727f8a1195406d20e564f1a12989356d93c62"/>
+                            {/* <img src={dataPiece.data.media_metadata[0].p[3].u}/> */}
                         </div>
                 )
             })}
