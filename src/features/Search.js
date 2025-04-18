b754a4c2f06eda1024a6ac0b79aa0c97bd411e13
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-18 10:52:14 +0200

    update Search component

--- src/features/Search.js
+++ src/features/Search.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate, createSearchParams } from 'react-router-dom';
 
 export default function Search () {
+
+    const [searchInput, setSearchInput] = useState('');
+    const navigate = useNavigate();
+    console.log('navigate', navigate);
+
+    function submitHandler() {
+
+    }
+
     return (
-        <div>
-            <input type="text" placeholder="Search"/>
-        </div>
+        <form onSubmit={submitHandler} className="searchForm">
+            <input onChange={e => setSearchInput(e.target.value)} className="search" type="text" placeholder="Search"/>
+            <button className="searchButton" type="submit">
+                🔍
+            </button>
+        </form>
     )
 }
