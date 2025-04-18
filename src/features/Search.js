53f09b7c9150fa14b70fc0754d353bf591840a10
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-18 17:01:57 +0200

    use query params to load the initial state of search input inside Search.js

--- src/features/Search.js
+++ src/features/Search.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useNavigate, createSearchParams, useParams } from 'react-router-dom';
+import { useNavigate, createSearchParams, useParams, useSearchParams} from 'react-router-dom';
 
 export default function Search () {
 
-    const [searchInput, setSearchInput] = useState('');
+    const [searchParams, setSearchParams] = useSearchParams();
+    console.log('searchParams', searchParams.get('q'));
+    const [searchInput, setSearchInput] = useState(searchParams.get('q'));
     const navigate = useNavigate();
     console.log('navigate', navigate);
-    const { subreddit } = useParams();
+    // const { subreddit } = useParams();
 
     function submitHandler(e) {
         e.preventDefault();
@@ -28,7 +30,11 @@ export default function Search () {
 
     return (
         <form onSubmit={submitHandler} className="searchForm">
-            <input onChange={e => setSearchInput(e.target.value)} className="search" type="text" placeholder="Search"/>
+            <input onChange={e => setSearchInput(e.target.value)} 
+                    className="search" 
+                    type="text" 
+                    placeholder="Search"
+                    value={searchInput}/>
             <button className="searchButton" type="submit">
                 🔍
             </button>
