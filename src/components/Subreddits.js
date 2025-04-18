f28c0f44dd3e800d1ee360ab2fef7b95190f2665
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-04-18 15:53:23 +0200

    import useSelector and selectedSubreddits into Subreddits.js and update its return statement so it renders the subreddits list from store.js

--- src/components/Subreddits.js
+++ src/components/Subreddits.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectedSubreddits } from '../app/subredditsSlice';
 
 export default function Subreddits(props) {
-    const [subreddits, setSubreddits] = useState(['TurtleFacts']);
+    // const [subreddits, setSubreddits] = useState(['TurtleFacts']);
+    const subreddits = useSelector(selectedSubreddits);
+    console.log('subreddits list rendered by Subreddits component', subreddits);
 
     return (
         <div className="subreddits">
             <h2>Subreddits</h2>
-            {subreddits.length > 0 && subreddits.map((subreddit, index) => {
+            {subreddits && subreddits.subreddits.map((subreddit, index) => {
                 console.log(subreddit);
                 return (
                     <Link to={subreddit}>
@@ -19,5 +23,18 @@ export default function Subreddits(props) {
                 )
             })}
         </div>
+        // <div className="subreddits">
+        //     <h2>Subreddits</h2>
+        //     {subreddits.length > 0 && subreddits.map((subreddit, index) => {
+        //         console.log(subreddit);
+        //         return (
+        //             <Link to={subreddit}>
+        //                 <li className="subreddit" 
+        //                     key={index} 
+        //                     >{subreddit}</li>
+        //             </Link>
+        //         )
+        //     })}
+        // </div>
     )
 }
